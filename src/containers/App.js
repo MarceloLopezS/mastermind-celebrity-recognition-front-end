a439c6f4835ce0b242a8788f04fd35c12e13aca1
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2023-01-21 12:06:17 -0500

    Add particles background

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
 import Navbar from '../components/Navbar/Navbar';
+import ParticlesBg from '../components/ParticlesBg/ParticlesBg';
 import UserValidation from '../pages/UserValidation';
 
 function App() {
-  return (
-    <React.StrictMode>
-        <Navbar />
-        <UserValidation />
-        <Footer />
-    </React.StrictMode>
-  );
+    return (
+        <React.StrictMode>
+            <ParticlesBg />
+            <Navbar />
+            <UserValidation />
+            <Footer />
+        </React.StrictMode>
+    );
 }
 
 export default App;
