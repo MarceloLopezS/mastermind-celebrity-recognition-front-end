b45a8bbe366f16af7cc51796ba149626946ab12b
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2023-02-18 12:48:42 -0500

    Add email verification page for notifications
    Server responses related to email verification now have dedicated
    routes in the front end.

---
+++ src/pages/EmailVerification/Success/Success.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Success = () => {
+    return (
+        <section className='container email-verification-section'>
+            <div>
+                <h1>Account verification successful</h1>
+                <p>You can now <span className='text-highlight'>log in to your account</span> and use face detection. Have fun!</p>
+            </div>
+        </section>
+    )
+}
+
+export default Success;
