bcde3217f4205119ca6dc1605a7480ababdc91d7
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2023-03-02 14:01:33 -0500

    Update message on email-verification/error page

--- src/pages/EmailVerification/Error/Error.js
+++ src/pages/EmailVerification/Error/Error.js
@@ -5,7 +5,7 @@ const Error = () => {
         <section className='container email-verification-section'>
             <div>
                 <h1>An error occured</h1>
-                <p><span className='text-highlight'>Something went wrong</span> during the activation process. Please try again later.</p>
+                <p><span className='text-highlight'>Something went wrong</span> during the activation process. Please verify that your link has not expired, or try again later.</p>
             </div>
         </section>
     )
