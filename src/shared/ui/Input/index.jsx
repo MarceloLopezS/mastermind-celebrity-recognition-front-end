dd6ef05b46730eaac03865e947c5828f15fefba6
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-05 17:35:41 -0500

    Add: Input component

---
+++ src/shared/ui/Input/index.jsx
@@ -0,0 +1,15 @@
+import { forwardRef } from "react"
+import styles from "./ui/styles.module.css"
+
+const Input = forwardRef(({ isValid, ...attributes }, ref) => {
+	return (
+		<input
+			ref={ref}
+			className={styles.input}
+			{...attributes}
+			data-invalid={!isValid ? true : null}
+		/>
+	)
+})
+
+export default Input
