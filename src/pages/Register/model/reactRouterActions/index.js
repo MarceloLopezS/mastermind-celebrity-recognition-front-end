f8dc188507672dfd04e84718a4c4ec833d420dd6
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-08 23:02:14 -0500

    Add: ReactRouterActions in model slice

---
+++ src/pages/Register/model/reactRouterActions/index.js
@@ -0,0 +1,33 @@
+import { redirect } from "react-router-dom"
+import { getReactRouterFormData } from "../../../../shared/utils/functions"
+import registerUser from "../../../../features/RegisterUser"
+
+export const submitRegisterForm = async ({ request }) => {
+  try {
+    const formData = await getReactRouterFormData(request)
+    const fetchOptions = {
+      method: request.method,
+      credentials: 'include',
+      body: formData,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const serverResponse = await registerUser(fetchOptions)
+
+    if (serverResponse.status === "success") {
+      return redirect("/email-verification")
+    }
+
+    return serverResponse
+  } catch (err) {
+    console.error(`Fetch error: ${err}`)
+    return {
+      status: "client-error",
+      clientError: {
+        message: 'We were not able to process the request. Please try again in a few moments.'
+      }
+    }
+  }
+}
