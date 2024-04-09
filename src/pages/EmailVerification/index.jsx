0378e0261f765be7b8816cafd75ae56d3c76483b
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-09 14:20:22 -0500

    Fix: Add / to path on loader redirect

--- src/pages/EmailVerification/index.jsx
+++ src/pages/EmailVerification/index.jsx
@@ -32,7 +32,7 @@ const EmailVerificationRoute = {
 				const userData = await getUserData()
 				if (!userData) return null
 
-				return redirect("face-detection")
+				return redirect("/face-detection")
 			}
 		},
 		EmailVerificationErrorRoute,
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -18,7 +18,7 @@ const RegisterRoute = {
 		const userData = await getUserData()
 		if (!userData) return null
 
-		return redirect("face-detection")
+		return redirect("/face-detection")
 	},
 	action: submitRegisterForm
 }
