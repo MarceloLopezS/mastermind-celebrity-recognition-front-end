2fee005ffd68f78a04d0e2e64fc368dc0765f9bd
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-10 19:28:19 -0500

    Add: DetectionBoundingBox entity

---
+++ src/entities/DetectionBoundingBox/index.jsx
@@ -0,0 +1,30 @@
+import { capitalizeString } from "../../shared/utils/functions"
+import styles from "./ui/styles.module.css"
+
+const DetectionBoundingBox = ({ detectionData }) => {
+	detectionData
+		? detectionData.map(detection => {
+				const { top_row, right_col, bottom_row, left_col } =
+					detection.boundingBox
+				const { name, probability } = detection.faceDetection
+				return (
+					<div
+						key={name}
+						className={styles["boundingBox"]}
+						style={{
+							top: `${top_row * 100}%`,
+							right: `${100 - right_col * 100}%`,
+							bottom: `${100 - bottom_row * 100}%`,
+							left: `${left_col * 100}%`
+						}}
+					>
+						<span className={styles["detection-name"]}>
+							{capitalizeString(name)} | {Math.floor(probability * 100)}%
+						</span>
+					</div>
+				)
+		  })
+		: null
+}
+
+export default DetectionBoundingBox
