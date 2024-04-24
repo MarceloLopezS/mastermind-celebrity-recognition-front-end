cee16c77a30b9ee765e1e2f7541a9f146c47191a
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-24 14:51:32 -0500

    Revert "Add: getUserData feature"
    This reverts commit 660b6841a3eee2f6a0bc5b668f4af8eb580cb024.

--- src/features/getUserData/api/index.js
+++
@@ -1,9 +0,0 @@
-import { SERVER_DOMAIN } from "../../../shared/utils/constants"
-
-const requestUserData = async (fetchOptions) => {
-  const response = await fetch(`${SERVER_DOMAIN}/user-info`, fetchOptions)
-
-  return response
-}
-
-export default requestUserData
