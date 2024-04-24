fc66e70c75ac2569a48d996169b97775a7d56b0d
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-24 13:31:32 -0500

    Update: Move server response processing to feature

--- src/features/LogoutUser/api/index.js
+++ src/features/LogoutUser/api/index.js
@@ -2,9 +2,8 @@ import { SERVER_DOMAIN } from "../../../shared/utils/constants"
 
 const requestLogout = async (fetchOptions) => {
   const response = await fetch(`${SERVER_DOMAIN}/logout`, fetchOptions)
-  const data = await response.json()
 
-  return data
+  return response
 }
 
 export default requestLogout
