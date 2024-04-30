7d85c87066e5c8aa0330d4ca4aa35f6ccfefcec8
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-04-29 19:22:57 -0500

    Update: Authentication check in loader function

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,4 +1,4 @@
-import { getUserData } from "../../controllers/ReactRouterLoaders/loaders"
+import checkUserAuthentication from "../../features/CheckUserAuthentication"
 import { submitRegisterForm } from "./model/reactRouterActions"
 import RegisterForm from "../../widgets/RegisterForm"
 import styles from "./ui/styles.module.css"
@@ -15,10 +15,15 @@ const RegisterRoute = {
 	path: "register",
 	element: <Register />,
 	loader: async () => {
-		const userData = await getUserData()
-		if (!userData) return null
+		try {
+			const data = await checkUserAuthentication()
+			if (!data?.authenticated) return null
 
-		return redirect("/face-detection")
+			return redirect("/face-detection")
+		} catch (err) {
+			console.error(err)
+			return null
+		}
 	},
 	action: submitRegisterForm
 }
