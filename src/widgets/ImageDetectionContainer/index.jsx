865951d673b259d2866160931acd0c9e10961f90
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-05-09 12:16:14 -0500

    Add: ImageDetectionContainer widget

---
+++ src/widgets/ImageDetectionContainer/index.jsx
@@ -0,0 +1,31 @@
+import DoubleSquareLoader from "../../shared/ui/DoubleSquareLoader"
+import DetectionBoundingBox from "../../entities/DetectionBoundingBox"
+import styles from "./ui/styles.module.css"
+
+const ImageDetectionContainer = ({
+	children,
+	isVisible = true,
+	isLoading = false,
+	detectionData,
+	errorMessage = "",
+	...attributes
+}) => {
+	return (
+		<div data-show={isVisible ? true : null} {...attributes}>
+			{children}
+			<DoubleSquareLoader
+				isShown={isLoading}
+				className={styles["detection-loader"]}
+			/>
+			<div
+				className={`${styles["error-message"]} | text-highlight`}
+				data-show={errorMessage ? true : null}
+			>
+				{errorMessage || null}
+			</div>
+			<DetectionBoundingBox detectionData={detectionData} />
+		</div>
+	)
+}
+
+export default ImageDetectionContainer
