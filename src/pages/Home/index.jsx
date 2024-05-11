36f5ea092c96e9152e4bf19abf3bd346496487d4
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-05-10 20:47:17 -0500

    Add: HeroDemo component to composition

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import HeroDescription from "./ui/HeroDescription"
+import HeroDemo from "./ui/HeroDemo"
 import styles from "./ui/styles.module.css"
 
 const Home = () => {
 	return (
 		<section className={`${styles["hero"]} | container`}>
 			<HeroDescription />
+			<HeroDemo />
 		</section>
 	)
 }
