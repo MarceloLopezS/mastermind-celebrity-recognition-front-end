b634da905b9d503876ffd4f5bf0dcda25366e12e
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-05-17 19:35:41 -0500

    Update: Add hero demo container element

--- src/pages/Home/ui/HeroDemo/index.jsx
+++ src/pages/Home/ui/HeroDemo/index.jsx
@@ -21,29 +21,37 @@ const HeroDemo = () => {
 	const [currentImageSrc, setCurrentImageSrc] = useState(DEMO_IMG_SOURCES[0])
 
 	return (
-		<section className={`${styles["hero__demo"]} | box-shadow--app-theme`}>
-			<section className={styles["demo-thumbnails--container"]}>
-				{DEMO_IMG_SOURCES.map((demoImgSrc, index) => (
-					<button
-						onClick={() => setCurrentImageSrc(demoImgSrc)}
-						type="button"
-						className={styles["demo-thumbnail"]}
-						tabIndex={demoImgSrc === currentImageSrc ? -1 : null}
-						data-selected={demoImgSrc === currentImageSrc ? true : null}
-					>
-						<span className="sr-only">{`Thumbnail number ${index + 1}`}</span>
-						<img
-							key={demoImgSrc}
-							src={demoImgSrc}
-							alt="Demo thumbnail"
-							draggable={false}
-						/>
-					</button>
-				))}
-			</section>
-			<ImageDetectionContainer className={styles["demo-detection--container"]}>
-				<img src={currentImageSrc} alt="Current demo image" draggable={false} />
-			</ImageDetectionContainer>
+		<section className={styles["hero__demo--container"]}>
+			<div className={`${styles["hero__demo"]} | box-shadow--app-theme`}>
+				<section className={styles["demo-thumbnails--container"]}>
+					{DEMO_IMG_SOURCES.map((demoImgSrc, index) => (
+						<button
+							onClick={() => setCurrentImageSrc(demoImgSrc)}
+							type="button"
+							className={styles["demo-thumbnail"]}
+							tabIndex={demoImgSrc === currentImageSrc ? -1 : null}
+							data-selected={demoImgSrc === currentImageSrc ? true : null}
+						>
+							<span className="sr-only">{`Thumbnail number ${index + 1}`}</span>
+							<img
+								key={demoImgSrc}
+								src={demoImgSrc}
+								alt="Demo thumbnail"
+								draggable={false}
+							/>
+						</button>
+					))}
+				</section>
+				<ImageDetectionContainer
+					className={styles["demo-detection--container"]}
+				>
+					<img
+						src={currentImageSrc}
+						alt="Current demo image"
+						draggable={false}
+					/>
+				</ImageDetectionContainer>
+			</div>
 		</section>
 	)
 }
