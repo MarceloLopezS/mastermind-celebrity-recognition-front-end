086a07d7e2af5d132add7b3ed8deb46a4cac44d8
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-05-21 17:37:48 -0500

    Add: useFaceDetectionDemo hook

---
+++ src/pages/Home/ui/HeroDemo/model/hooks/index.js
@@ -0,0 +1,58 @@
+import { useState, useCallback, useMemo, useEffect } from "react"
+import getDemoDetectionData from "../../../../../../features/GetDemoDetectionData"
+
+const DEFAULT_DETECTION_ERROR =
+  "There was a problem retrieving the detection data."
+
+export const useFaceDetectionDemo = (demoThumbnailsArr = []) => {
+  const FIRST_DEMO_THUMBNAIL = useMemo(() => demoThumbnailsArr.reduce((acc, thumbnail) =>
+    thumbnail?.demoId === 0 ? thumbnail : acc
+  ), [demoThumbnailsArr])
+
+  const [currentImageSrc, setCurrentImageSrc] = useState(
+    FIRST_DEMO_THUMBNAIL?.src
+  )
+  const [isRequestLoading, setIsRequestLoading] = useState(false)
+  const [detectionData, setDetectionData] = useState(null)
+  const [detectionError, setDetectionError] = useState(null)
+
+  const handleDetectionDataRecieval = useCallback(detectionData => {
+    setDetectionData(detectionData)
+    setDetectionError(null)
+  }, [])
+
+  useEffect(() => {
+    const handleThumbnailDetection = async demoId => {
+      try {
+        setIsRequestLoading(true)
+
+        const data = await getDemoDetectionData(demoId)
+
+        if (data.status === "success") {
+          handleDetectionDataRecieval(data.detectionData)
+        }
+
+        if (data.status === "fail") setDetectionError(DEFAULT_DETECTION_ERROR)
+
+        setIsRequestLoading(false)
+      } catch (err) {
+        console.error(`Fetch error: ${err}`)
+        setIsRequestLoading(false)
+        setDetectionError(DEFAULT_DETECTION_ERROR)
+      }
+    }
+
+    const currentDemo = demoThumbnailsArr.reduce((acc, thumbnail) =>
+      thumbnail?.src === currentImageSrc ? thumbnail : acc
+    )
+    handleThumbnailDetection(currentDemo?.demoId)
+  }, [currentImageSrc])
+
+  return {
+    currentImageSrc,
+    setCurrentImageSrc,
+    isRequestLoading,
+    detectionData,
+    detectionError
+  }
+}
