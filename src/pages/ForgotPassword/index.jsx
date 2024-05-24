8dc631a7a183491a3d83d485b7993716ee544d98
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-05-24 11:38:54 -0500

    Update(ForgotPassword): Refactor auth based redirection logic

--- src/pages/ForgotPassword/index.jsx
+++ src/pages/ForgotPassword/index.jsx
@@ -1,4 +1,4 @@
-import checkUserAuthentication from "../../features/CheckUserAuthentication"
+import WithAuthRedirection from "../../widgets/WithAuthRedirection"
 import ForgotPasswordForm from "../../widgets/ForgotPasswordForm"
 import { submitForgotPasswordForm } from "./model/ReactRouterActions"
 import styles from "./ui/styles.module.css"
@@ -13,20 +13,21 @@ const ForgotPassword = () => {
 	)
 }
 
+const WithAuthRedirectionForgotPassword = () => {
+	return (
+		<WithAuthRedirection
+			resolveRedirectPath={isUserAuthenticated =>
+				isUserAuthenticated ? "/face-detection" : null
+			}
+		>
+			<ForgotPassword />
+		</WithAuthRedirection>
+	)
+}
+
 const ForgotPasswordRoute = {
 	path: "forgot-password",
-	element: <ForgotPassword />,
-	loader: async () => {
-		try {
-			const data = await checkUserAuthentication()
-			if (!data?.authenticated) return null
-
-			return redirect("/face-detection")
-		} catch (err) {
-			console.error(err)
-			return null
-		}
-	},
+	element: <WithAuthRedirectionForgotPassword />,
 	action: submitForgotPasswordForm
 }
 
