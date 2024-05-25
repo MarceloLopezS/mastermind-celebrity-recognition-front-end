813018aa0be25c67c5a12871b597144bc6eeff5f
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-05-25 10:00:26 -0500

    Add: PATHNAMES constant

--- src/shared/utils/constants/index.js
+++ src/shared/utils/constants/index.js
@@ -1,1 +1,14 @@
 export const SERVER_DOMAIN = "https://face-detection-app-back-end.fly.dev"
+export const PATHNAMES = {
+  REGISTER: "register",
+  EMAIL_VERIFICATION: "email-verification",
+  ACTIVATION_SUCCESS: "activation-success",
+  INVALID_TOKEN: "invalid-token",
+  ERROR: "error",
+  FORGOT_PASSWORD: "forgot-password",
+  PASSWORD_RESET: "password-reset",
+  RESET_SUCCESS: "reset-success",
+  LOGIN: "login",
+  FACE_DETECTION: "face-detection",
+  LOGOUT: "logout",
+}
