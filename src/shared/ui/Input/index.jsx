60fb6b665e85b74b3da1a5142f780b193553af37
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2025-01-07 22:06:48 -0500

    delete(Input): forwardRef

--- src/shared/ui/Input/index.jsx
+++ src/shared/ui/Input/index.jsx
@@ -1,15 +1,14 @@
-import { forwardRef } from "react"
 import styles from "./ui/styles.module.css"
 
-const Input = forwardRef(({ isValid = true, ...attributes }, ref) => {
-	return (
-		<input
-			ref={ref}
-			className={styles.input}
-			{...attributes}
-			data-invalid={!isValid ? true : null}
-		/>
-	)
-})
+const Input = ({ isValid = true, ref, ...attributes }) => {
+  return (
+    <input
+      ref={ref}
+      className={styles.input}
+      {...attributes}
+      data-invalid={!isValid ? true : null}
+    />
+  )
+}
 
 export default Input
